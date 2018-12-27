8cded99cb7262f7bc08aa1d790cb5b78efd3e0f2
MohanrajS19 <mohanraj.sambath@ibotsystems.com>
2018-12-27 16:03:17 +0530

    Intial Commit with TestAPI integrated for recycler with POJO class.

---
+++ app/src/main/java/app/movie/tutorial/com/Config.java
@@ -0,0 +1,9 @@
+package app.movie.tutorial.com;
+
+public class Config {
+    public static String PACKAGE_NAME = "app.movie.tutorial.com";
+    public final static String IMAGE_URL_BASE_PATH = "http://image.tmdb.org/t/p/w342//";
+    public static final String API_BASE_URL = "http://api.themoviedb.org/3/";
+    public final static String API_KEY = "0ad2a8e78ba1a2796fc424f01600d3de";
+
+}
